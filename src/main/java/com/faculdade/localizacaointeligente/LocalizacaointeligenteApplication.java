8346240a75b0ec00f29ad38a61f5afe7c2e69971
edvan <edvanilson000@gmail.com>
2025-04-19 23:13:55 -0300

    chore: estrutura inicial do projeto

---
+++ src/main/java/com/faculdade/localizacaointeligente/LocalizacaointeligenteApplication.java
@@ -0,0 +1,13 @@
+package com.faculdade.localizacaointeligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalizacaointeligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalizacaointeligenteApplication.class, args);
+	}
+
+}
