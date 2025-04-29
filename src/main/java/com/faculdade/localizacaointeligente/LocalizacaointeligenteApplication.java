91283e7dfea068813bf82c32b51b44e077574297
edvan <edvanilson000@gmail.com>
2025-04-29 20:45:42 -0300

    feat(criacao-do-crud): adicionar configuração de RestTemplate com suporte a conversão JSON

--- src/main/java/com/faculdade/localizacaointeligente/LocalizacaointeligenteApplication.java
+++ src/main/java/com/faculdade/localizacaointeligente/LocalizacaointeligenteApplication.java
@@ -2,12 +2,13 @@ package com.faculdade.localizacaointeligente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.faculdade.localizacaointeligente")
 public class LocalizacaointeligenteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LocalizacaointeligenteApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(LocalizacaointeligenteApplication.class, args);
+    }
 }
